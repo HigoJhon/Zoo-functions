84fbb93122abb2a8e8fce346bd513e8846f24469
Higo Jhon <higojhon98@gmail.com>
2022-09-29 17:58:59 -0300

    buscando as espécies dos animais por meio de um ID

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((xux) => {
+    if (ids === 'undefined') {
+      return [];
+    } return ids.includes(xux.id);
+  });
 }
-console.log(getSpeciesByIds());
+// ids === 'undefined' ? [] : ids.includes(xux.id)
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
