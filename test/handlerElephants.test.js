36ac1215844fbc918e0f7748372c81599c991c4a
Higo Jhon <higojhon98@gmail.com>
2022-10-03 22:44:19 -0300

    80% de cobertura

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,14 @@
+const { species } = require('../data/zoo_data');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Essa função retorna informações referentes aos elefantes conforme o argumento passado', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('testando name', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('testando averageAge', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
