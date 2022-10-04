ba76748483b124cfdaa7bf4261f97d342cb6efed
Higo Jhon <higojhon98@gmail.com>
2022-10-04 20:52:36 -0300

    Encontar animal mais velho

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const nomeAni = employees.find((a) => a.id === id).responsibleFor[0];
+  const idade = species.find((b) => b.id === nomeAni).residents;
+  const resuls = idade.sort((a, b) => b.age - a.age)[0];
+  return Object.values(resuls);
 }
 
 module.exports = getOldestFromFirstSpecies;
