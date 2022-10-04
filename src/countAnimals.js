b2c3f1ac5c8c3817db6f7c3cd04d96a3aded694f
Higo Jhon <higojhon98@gmail.com>
2022-10-03 22:06:20 -0300

    animais contabilizados por espécie

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,39 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// const [rest1, rest2, rest3, rest4, rest5, rest6, rest7, rest8] = species;
+
+// console.log(idss);
+// const [...test1] = species;
+// console.log(residents)
+// // const um = test1.
+// console.log(tes.id);
+// console.log(rest2);
+// console.log(rest3);
+// console.log(rest);
+
+// console.log(`${popularity}`);
+// console.log(popularity);
 function countAnimals(animal) {
-  // seu código aqui
+  // species.reduce((acum, p) => {
+  //   if (animal === undefined);
+  //   return {}`${acum.name}:${p.residents.length}`;
+  // }, {});
+  // (animal === a.name : a.residents.length));
+
+  if (animal === undefined) {
+    const resul = species.map((a) => ({ [a.name]: a.residents.length })).sort();
+    return Object.assign({}, ...resul);
+  }
+  const save = species.find((b) => (animal.specie === b.name));
+  const save2 = save.residents.filter((c) => animal.sex === c.sex);
+  if (!animal.sex) {
+    return save.residents.length;
+  }
+  if (animal.sex) {
+    return save2.length;
+  }
 }
 
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
